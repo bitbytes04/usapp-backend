57386a012c12f56f14b0a1116c17a109d66f3ab8
dwit729 <124338784+dwit729@users.noreply.github.com>
2025-06-03 09:58:14 +0800

    trying to bug fix

--- firebase/config.js
+++ firebase/config.js
@@ -1,9 +1,11 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("./firebaseServiceAccount.json");
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+    });
+}
 
 const db = admin.firestore();
 const storage = admin.storage().bucket();
