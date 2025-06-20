faac7e1cbcb05861497839c8186c9c866cb08ae7
dwit729 <124338784+dwit729@users.noreply.github.com>
2025-06-20 10:51:40 +0800

    added edit to slp account

--- routes/slpRoutes.js
+++ routes/slpRoutes.js
@@ -8,6 +8,7 @@ router.get("/board-usage-summary/:uid/", controller.getBoardUsageSummary);
 router.get("/:uid/linked-users", controller.getLinkedUsers);
 router.post("/link-request/:uid", controller.postLinkRequest);
 router.post("/remove-link/:uid/:linkedUserId", controller.removeLinkedUser);
+router.post("edit-slp/:uid", controller.editSLPUser);
 
 
 module.exports = router;
