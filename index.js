02ec59c9e303ac7326aa1aadc4ed6912721fd450
dwit729 <124338784+dwit729@users.noreply.github.com>
2025-09-07 19:23:26 +0800

    security patch

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const cors = require("cors");
+const helmet = require('helmet');
 const userRoutes = require("./routes/userRoutes");
 const defaultRoutes = require("./routes/defaultRoutes");
 const boardRoutes = require("./routes/boardRoutes");
 const slpRoutes = require("./routes/slpRoutes")
 const adminRoutes = require("./routes/adminRoutes")
 
+const allowedFrameAncestors = ['https://usapp-aac.org'];
 const app = express();
 
 app.use(cors());
+
+app.use(helmet.referrerPolicy({ policy: 'no-referrer' }));
+
+app.use(
+    helmet({
+        contentSecurityPolicy: {
+            directives: {
+                frameAncestors: ["'self'", ...allowedFrameAncestors], // Allow framing from own origin and specified URLs
+            },
+        },
+        xFrameOptions: false, // Disable the deprecated X-Frame-Options header since Content-Security-Policy is used
+    })
+);
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
